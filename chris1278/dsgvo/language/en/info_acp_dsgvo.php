f420e48a748ac102c039258547f90c65ca18f56b
chris1278 <31364765+chris1278@users.noreply.github.com>
2021-11-22 01:30:48 +0100

    Add files via upload

--- chris1278/dsgvo/language/en/info_acp_dsgvo.php
+++ chris1278/dsgvo/language/en/info_acp_dsgvo.php
@@ -20,7 +20,7 @@ if (empty($lang) || !is_array($lang))
 $lang = array_merge($lang, [
 	// Information For Versionscheck Metadaten
 	'DSGVO_LANG_DESC'							=>	'English (en)',
-	'DSGVO_LANG_EXT_VER' 						=>	'1.0.0',
+	'DSGVO_LANG_EXT_VER' 						=>	'1.0.1',
 	'DSGVO_LANG_AUTHOR' 						=>	'Chris1278',
 	'DSGVO_DESC' 								=>	'Here the settings for the extension „%1$s“ (v%2$s) can be changed.',
 	'DSGVO_MSG_LANGUAGEPACK_OUTDATED'			=>	'Note: The language pack for this extension is no longer up-to-date',
